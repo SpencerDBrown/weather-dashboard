256c813b3ca9ac0087cb076b5639262fb812b0b5
Spencer <spencerdbrown@comcast.net>
2022-08-09 20:18:53 -0400

    added initial defined variabled of const / let, also added API key

--- asset/script.js
+++ asset/script.js
@@ -1,3 +1,10 @@
-function initPage() {
+function init () {
+    const cityEl = document.getElementById("input-city");
+    const nameEl = document.getElementById("city-name");
+    const searchEl = document.getElementById("primary-btn");
+    const clearEl = document.getElementById("clear");
+    const currentTempEl = document.getElementById("temp");
+    const currentHumEl = document.getElementById("humidity");
+    const currentWindEl = document.getElementById("wind-speed");
+    const currentUvEl = document.getElementById("UV-index");
 }
-    const APIKey = ""
